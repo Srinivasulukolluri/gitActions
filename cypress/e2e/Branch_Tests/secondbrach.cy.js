688769b86a97a63e8283228bd1f14e751b18be32
srinivasulukolluri <srinivasulukolluri@hotmail.com>
2024-01-07 20:26:39 -0500

    new test

---
+++ cypress/e2e/Branch_Tests/secondbrach.cy.js
@@ -0,0 +1,16 @@
+import secondPage from "../../pages/secondpage.js"
+
+const secondpage = new secondPage();
+
+describe('second branch - jenkins test',()=>{
+
+    it('',()=>{
+
+        cy.visit("https://demo.guru99.com/V1/")
+        // secondpage.bankProject();
+        // secondpage.SendCredentials()
+        // secondpage.sendPaword()
+        // secondpage.ClickLogin()
+
+    })
+})
